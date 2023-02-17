49cf54a474f44a85a0d0579705435bc9a6f684c7
Navneet Vikram Tey <navneetvikramtey@gmail.com>
2023-02-17 22:05:22 +0530

    Added Everything

---
+++ README.md
@@ -0,0 +1,121 @@
+# bash_vault
+
+Combination of Cryptsetup+Zenity+Bash to created an encrypted Vault , with some extra perks !!
+
+## Pre-requisite
+
+![Installed App](./source/prerequisite.png "Check if you have these installed")
+
+## Configuration
+
+There is nothing to configure as of now except
+
+1. **MAX_VAULT_SIZE** by default on line #6 of the file, it's **30** ( *as in 30GB*), you can change it to whatever you seem fit
+2. **VAULT_FOLDER** by default on line #7 it's **.vaults** which equated to ``` /home/user/.vaults ```, to change it to ``` /home/user/secret/whatever ``` change line #7 to **VAULT_FOLDER="secret/whatever"** , make sure folder name ``` whatever ``` doesn't exist
+
+## Why This?
+
+1. Easy to implement, doesn't require any other program which already doesn't come installed on many distro except zenity 
+2. Here my "Homework" vault is 20GB (which can be extended to maximum depending upon **MAX_VAULT_SIZE**) If i have to backup it, I need to copy just 1 file which is 20GB, in contrast to gocryptfs where there are so many files and it slows down my backup speed.
+
+## Why Shouldn't I use it? 
+
+1. Every vault is secured with a password, you lose it and you lose the vault :"( 
+2. If gocryptfs/any other encrypted software works for you, then don't fix what isn't broke ( although would love to see people trying this out)
+3. Can't decrease the size of vault, only can increase it ( but there is a workaround )
+
+## Walk me through these Zenity prompts?
+
+1. Always your first prompt! Enter your sudo password. Why? Requires access to /dev/mapper to mount devices, you will be asked for sudo once during whole script lifecycle , to avoid any annoyance <3
+![Installed App](./source/sudo.png "Your First Prompt, Asking for Sudo Password")  
+
+2. What should script do next? 
+    - If **VAULT_FOLDER** was not present , create it and then ask to create a new vault with given name and password, when creating vault **don't put .img at end** it will be done automatically
+     ![Installed App](./source/create_vault.png "If your Sudo password is correct, On first run ") 
+    - If **VAULT_FOLDER** was present, and no vaults were found ( because you deleted it), if pressed yes, it will give prompt as above
+     ![Installed App](./source/no_vault.png "If your Sudo password is correct, And you have no vaults, check vault folder to see if you have vaults?")
+    - If **VAULT_FOLDER** was present, and there are more than 0 vaults (vaults end with .img) it asks you to choose action for certain vault, check [Understanding default prompt](#understanding-default-prompt) 
+     ![Installed App](./source/default.png "If your Sudo password is correct, And you have no vaults, check vault folder to see if you have vaults?")
+
+## Understanding default Prompt
+
+### Understanding Buttons 
+
+1. Exit And Close -> Script will exit , and close all vaults ( Pressing Esc button would do same)
+2. Exit and Wait -> Script will wait in background . 
+   - to make it exit 
+    ``` echo closeall >> /home/$USER/<vault_folder>/pipe/m_pipe ```<br>
+    default ``` echo closeall >> /home/$USER/.vaults/pipe/m_pipe ```
+
+   - to show default prompt again 
+    ``` echo okay >> /home/$USER/<vault_folder>/pipe/m_pipe ```<br>
+    default ``` echo closeall >> /home/$USER/.vaults/pipe/m_pipe ```
+
+3. Create New -> Create new vault 
+4. Okay button -> Takes input of Action and Vault, if no valid Action/Vault is provided, gives error
+
+### Understanding Action 
+
+1. Open -> Asks for password of said vault and mounts it in **VAULT_FOLDER/Random_UUID**, and then xdg-open it 
+2. Close -> If vault is open then close it 
+3. Delete -> Close vault if opened and then proceed to delete it 
+4. Extend -> Show a scale between current SIZE of vault and **MAX_VAULT_SIZE** , ask user to choose 
+
+In case of error/success Strings are updated and are shown here
+![Installed App](./source/info_action.png "See the highlight in red sqaure!")
+
+## What If
+
+1. I ran bash_vault.sh from Terminal, What if Ctrl+C it, Do Vault Remain Open?? What happens?
+   <br>
+   No, signal like SIGINT is trapped and is used to close all vaults before exiting.
+2. I am copying data to vault and i force closed it, what happens to my data? 
+   <br>
+   Depends on resilience of ext4 itself
+3. I like how it works but my work is around one vault only, I don't need many :"( <br>
+   <br>
+   This is where power of scripting comes in , and you can use keybinding for that.
+   For example in following script you can toggle mount by running it 
+    ```bash 
+    #!/bin/bash
+    VAULT="testing.img"  # Remember to put .img here at end, Set your default vault  
+    SCRIPT_LOCATION="/home/$USER/bash_vault/bash_vault.sh" #Location of your bash_vault script 
+    VAULT_FOLDER=".vaults" #Change it only if you have changed default in script 
+   
+    USER_R=$USER
+    FOLDER="/home/$USER/$VAULT_FOLDER"
+    PIPE="$FOLDER/pipe/m_pipe"
+    V_PIPE="$FOLDER/pipe/$VAULT"
+     
+    if [ -p $V_PIPE ]; then 
+        echo "Closing vault"
+        echo close >> $V_PIPE 
+    else
+       PASS=$(zenity  --password --title="Enter Sudo Password") 
+       echo $PASS | sudo -E -S $SCRIPT_LOCATION $USER_R $VAULT
+    fi
+    ```
+## Demonstration 
+
+1. Creating a new vault , and you can see force exit will close all mounts ( and so will rebooting your system)
+
+![Video](./source/create_vault_and_foce_close.gif)
+
+2. Extending the newly created vault from 1GB to 5GB 
+   
+![Video](./source/extend_video.gif)
+
+3. Closing the vaults using script or zenity window
+
+![Video](./source/closing_vault.gif)
+
+4. Demonstration of my setup where bash_vault is bound to ALT+SHIFT+V `sh -c "~/bash_vault/bash_vault.sh"`
+
+![Video](./source/my_setup.gif)
+
+5. Demonstration of script mentioned in [What if](#what-if) section, which toggles mount of `testing.img` Vault on my machine
+
+![Video](./source/example.gif)
+
+Donations? , [PayPal](https://paypal.me/TalentedTey?locale.x=en_GB)
+Suggestions? , [Signal](https://signal.me/#p/+919519873721)
---
+++ bash_vault.sh
@@ -0,0 +1,374 @@
+#!/bin/bash
+## Author : Navneet Vikram Tey (teynav)
+## Github : @teynav 
+## Bugs?  : https://github.com/teynav/bash_vault/issues
+#
+MAX_VAULT_SIZE=30
+VAULT_FOLDER=".vaults"
+
+USER_I=$(whoami)
+FOLDER="/home/$USER_I/$VAULT_FOLDER"
+PIPE="$FOLDER/pipe"
+M_PIPE="$FOLDER/pipe/m_pipe"
+VAULTS=""
+MKFS="mkfs.ext4"
+SHOWN_INFO_ONCE=0
+DONT_CHANGE_WELCOME=0
+FILE_E=$(realpath $0)
+if [[ -p $M_PIPE ]]; then
+    echo okay > $M_PIPE
+    exit 0
+fi 
+function close {
+    if [[ "$JUSTCLOSE" == "yes" ]];then 
+        exit 0 
+    fi
+    if [[ ! -d $PIPE ]];then 
+        exit 0
+    fi 
+    cd $PIPE 
+    for pipes in *.img
+    do
+        echo "close" > $pipes 
+    done 
+    cd ..
+    rm -rf $PIPE 
+    exit 0
+}
+function dorightthing {
+    files=$(ls -al $PIPE/*.img | wc -l )
+    if [[ "$files" == "0" ]];then
+        zenity --title="Vaults" --info --title="Exit" --text="None of the vaults are opened" --timeout=10
+        close 
+    else 
+        if [[ "$1" == "" ]];then
+            if [[ "$SHOWN_INFO_ONCE" == "0" ]];then 
+                SHOWN_INFO_ONCE=1
+                zenity --title="Vaults" --info --title="Waiting in Background" --text="To close all vaults \"echo closeall > $M_PIPE \"" --timeout 10
+            fi 
+        fi
+        while true;do 
+            read line < $M_PIPE
+            echo "Received $line"
+            if [[ "$line" == "closeall" ]];then 
+                close 
+                exit 0
+            elif [[ "$line" == "okay" ]];then 
+                return 0
+            elif [[ "$line" = Error:* ]];then 
+                zenity --title="Vaults" --warning --text="$line" --timeout=10
+                return 0
+            fi 
+        done
+    fi 
+}
+function waitonthis {
+    while true;do
+        sleep 5
+    done
+}
+function closethis {
+    rm -rf "$PIPE/$VAULTS"
+    umount "$FOLDER/$UUID" &>/dev/null 
+    rm -rf "$FOLDER/$UUID" &>/dev/null 
+    cryptsetup luksClose "$UUID" &>/dev/null
+    exit 0
+}
+function createvault {
+    d_input=$(zenity --title="Vaults" --forms --add-entry="Name Of Vault: " --text="Create your vault" --add-password="Password of Vault" --add-combo="Size(Default: 1024M)" --combo-values="1024M|2048M|5G|10G|20G")
+    SAVEIFS=$IFS && IFS=$'|' && d_input=($d_input) ; IFS=$SAVEIFS
+    nameofvault="$(echo ${d_input[0]} | xargs )"
+    pass="$(echo ${d_input[1]} | xargs )"
+    size="$(echo ${d_input[2]} | xargs )"
+    if [[ "$size" == "" ]];then 
+        size="1024M"
+    fi 
+    if [[ "$pass" == "" ]];then 
+        Welcome="You forgot to put enter password for new vault"
+        DONT_CHANGE_WELCOME=1
+        zenity --title="Vaults" --info --text="Incomplete info was given, no Password provided" --title="Error"
+    elif [[ "$nameofvault" == "" ]];then 
+        Welcome="You forgot to put Name of Vault"
+        DONT_CHANGE_WELCOME=1
+        zenity --title="Vaults" --info --text="Incomplete info was given, no vault name was given" --title="Error"
+    elif [ -f "$FOLDER/$nameofvault.img" ];then 
+        zenity --title="Vaults" --info --text="Vault Already Exists" --title="Error"
+        Welcome="Try with different vault name !!"
+        DONT_CHANGE_WELCOME=1
+    else 
+        echo "Creating your vault..."
+        NID=$(notify-send  -p "Starting to create your vault")
+        dd if=/dev/zero of="$FOLDER/$nameofvault.img" bs=1 count=0 seek=$size 
+ #       VAULTS="$VAULTS:$FOLDER/$nameofvault.img"
+        echo "Now setup a password to $nameofvault"
+        echo -n $pass | cryptsetup  luksFormat "$FOLDER/$nameofvault.img" -d -
+        NID=$(notify-send -r $NID -p "Formatting Vault with LUKS ")
+        UUID="$(uuidgen)"
+        echo "Setting up your vault, you will be asked for password again!!!"
+        echo $PASS | sudo -E -S sh -c "echo -n $pass | cryptsetup luksOpen \"$FOLDER/$nameofvault.img\" \"$UUID\" -d -"
+        echo 40
+        NID=$(notify-send -r $NID -p "Create new filesystem on $nameofvault.img")
+        echo $PASS | sudo -S  $MKFS "/dev/mapper/$UUID" 
+        echo 70
+        NID=$(notify-send -r $NID  -p "Wrapping UP")
+        echo $PASS | sudo -S  cryptsetup luksClose "$UUID"
+        echo "Your vault has been created"
+        Welcome="$nameofvault.img has been created"
+        DONT_CHANGE_WELCOME=1
+        echo 100
+        fi
+    }
+
+    function newinstall {
+        mkdir $FOLDER
+        createvault 
+    }
+
+    function open {
+        UUID=$(uuidgen)
+        trap closethis  SIGTERM SIGINT EXIT
+        if [ ! -d $PIPE  ];then 
+            mkdir $PIPE  
+            mkfifo $PIPE/$VAULTS
+            chown -R $USER_I:$USER_I "$FOLDER/pipe"
+        elif [ ! -p $PIPE/$VAULTS ];then
+            mkfifo $PIPE/$VAULTS
+            chown -R $USER_I:$USER_I "$FOLDER/pipe"
+        fi 
+        mkdir "$FOLDER/$UUID" 
+        pass="$(zenity --title="Vaults" --title="Your Vault = $VAULTS"  --password)"
+        echo -n $pass | cryptsetup luksOpen "$FOLDER/$VAULTS" "$UUID" - 1> /dev/null
+        sucess=$?
+        if [[ "$sucess" != "0" ]];then
+            if [ -p $M_PIPE ];then 
+            echo -e "Error: Bad password for $VAULTS \n Please Try Again" > $M_PIPE
+            else 
+            zenity --title="Vault Error" --info --text="Error: Bad password for $VAULTS \n Please Try Again"  
+            fi 
+            closethis 
+        fi 
+        mount /dev/mapper/$UUID "$FOLDER/$UUID"
+        sucess=$?
+        if [[ "$sucess" != "0" ]];then
+            if [ -p $M_PIPE ];then 
+                echo -e "Error: Damaged $VAULTS, Needs to be deleted" > $M_PIPE
+            else 
+               zenity --title="Vault Error" --info --text="Error: Damaged $VAULTS \n Need to be deleted"  
+            fi 
+            closethis
+        fi 
+        chown -R $USER_I:$USER_I "$FOLDER/$UUID"
+        (sudo -u $USER_I xdg-open "$FOLDER/$UUID" &>/dev/null) & disown
+        if [ -p $M_PIPE ];then 
+             echo okay > $M_PIPE
+        fi 
+        while true; do
+            echo "IN loop"
+            read line < $PIPE/$VAULTS
+            if [[ "$line" == "close" ]];then
+                closethis
+                exit 0
+            else 
+                sudo -u $USER_I xdg-open "$FOLDER/$UUID"
+            fi
+        done 
+    }
+
+    function display {
+        echo "Please select what you want to open"
+        A_VAULT=""
+        for elem in *.img 
+        do 
+            if [[ "$A_VAULT" == "" ]];then 
+                A_VAULT=$elem
+            else 
+                A_VAULT="$A_VAULT|$elem"
+            fi 
+        done
+        if [[ "$A_VAULT" == "*.img" ]];then
+            zenity --title="Vaults" --question --text="No Vault Found, Create One?"
+            result=$?
+            if [[ "$result" == "0" ]];then 
+                createvault
+            else 
+                close 
+            fi
+        else 
+            d_input=$(zenity --title="Vaults"  --text="$Welcome" --forms --add-combo="Action" --combo-values="Open|Close|Delete|Extend" --add-combo="Choose Vault" --combo-values="$A_VAULT" --show-header --extra-button "Create new" --cancel-label="Exit & Close" --extra-button "Exit & Wait")
+            returncode=$?
+            if [[ "$d_input" == "Create new" ]]; then 
+                createvault
+            elif [[ "$d_input" == "Exit & Wait" ]];then
+                dorightthing
+            elif [[ "$d_input" == "" ]]; then
+                close 
+            else  
+                SAVEIFS=$IFS && IFS=$'|' && d_input=($d_input) ; IFS=$SAVEIFS
+                action="$(echo ${d_input[0]} | xargs )"
+                vault_a="$(echo ${d_input[1]} | xargs )"
+                if [[ $action == "" ]] || [[ "$vault_a" == "" ]];then 
+                    Welcome="Both Action and Vault Name Required"
+                    DONT_CHANGE_WELCOME=1
+                elif [[ $action == "Open" ]];
+                then
+                    if [ -p "$PIPE/$vault_a" ];then 
+                        echo open >> $PIPE/$vault_a
+                    else 
+                        mkfifo "$PIPE/$vault_a"
+                        echo $PASS | sudo -S "$FILE_E" $USER_I $vault_a & disown
+                        dorightthing 1
+                        O_VAULT+=($!)
+                        sleep 2
+                                           
+                    fi 
+                        if [ -p "$PIPE/$vault_a" ];then 
+                            Welcome="$vault_a has been opened"
+                            DONT_CHANGE_WELCOME="1"
+                        else
+                            Welcome="$vault_a couldn't be opened"
+                            DONT_CHANGE_WELCOME="1"
+                        fi 
+                elif [[ "$action" == "Close" ]];then
+                        if [ -p "$PIPE/$vault_a" ];then 
+                            Welcome="$vault_a has been closed"
+                            echo close >> "$PIPE/$vault_a"
+                            DONT_CHANGE_WELCOME="1"
+                        else
+                            Welcome="$vault_a wasn't opened, Select Open to Open"
+                            DONT_CHANGE_WELCOME="1"
+                        fi 
+                elif [[ "$action" == "Delete" ]];then
+                    zenity --title="Vaults" --question --text="Are you sure to delete $vault_a?"
+                    response=$?
+                    if [[ "$response" == "1" ]]; then
+                        echo "Deletion cancelled"
+                        Welcome="Vault $vault_a was NOT deleted"
+                        DONT_CHANGE_WELCOME=1
+                    elif [ -p $PIPE/$vault_a ];then 
+                        echo close >> $PIPE/$vault_a
+                        sleep 3
+                        rm -rf $FOLDER/$vault_a
+                        Welcome="Vault $vault_a has been deleted"
+                        DONT_CHANGE_WELCOME=1
+                    else 
+                        rm -rf $FOLDER/$vault_a
+                        Welcome="Vault $vault_a has been deleted"
+                        DONT_CHANGE_WELCOME=1
+                    fi
+                elif [[ "$action" == "Extend" ]];then
+                    sizern=$(du --apparent-size $vault_a | sed -e "s/$vault_a//g")
+                    olds=$(( $sizern / 1024 / 1024 ))
+                    echo Current Size "$olds"G
+                    news=$(zenity --scale  --text="Choose new Size in GB" --min-value=$olds --value=$olds --max-value=$MAX_VAULT_SIZE  --step=2)
+                    sucess=$?
+                    if [ -p $PIPE/$vault_a ];then 
+                        echo close >> $PIPE/$vault_a
+                        sleep 3
+                    fi 
+                    echo $news
+                    if [[ "$news" == "$olds" ]];then 
+                        zenity --title="Vaults" --info --text="Old Size is equal to new size, Exiting"
+                        Welcome="Enter a different size for $vault_a"
+                        DONT_CHANGE_WELCOME=1
+                    elif [[ "$sucess" != "0" ]]; then
+                        Welcome="Extending $vault_a was cancelled"
+                        DONT_CHANGE_WELCOME=1
+                    else 
+                        pass="$(zenity  --title="Opening $vault_a" --text="Please enter password for $vault_a" --password)"
+                        UUID=$(uuidgen)
+                        echo $PASS | sudo -E -S sh -c "echo -n $pass | cryptsetup luksOpen \"$FOLDER/$vault_a\" \"$UUID\" -d -"
+                        sucess=$?
+                        if [[ "$sucess" != "0" ]];then 
+                            zenity --title="Vaults" --info --text="You entered wrong password"
+                        else
+
+                            NID=$(notify-send -p "First Closing Up Everything")
+                            echo $PASS | sudo -S  cryptsetup luksClose "$UUID"
+                            echo 20
+                            dd if=/dev/zero of=$vault_a bs=1 count=0 seek="$news"G
+                            NID=$(notify-send  -r $NID -p "Opening and Trying to Resize")
+                            echo $PASS | sudo -E -S sh -c "echo -n $pass | cryptsetup luksOpen \"$FOLDER/$vault_a\" \"$UUID\" -d -"
+                            echo 40
+                            echo $PASS | sudo -E -S sh -c "echo -n $pass | cryptsetup resize  \"$UUID\" -d -"
+                            echo 60
+                            NID=$(notify-send  -r $NID -p "Resizing Filesystem")
+                            echo $PASS | sudo -E -S e2fsck -fy  /dev/mapper/$UUID
+                            echo 80
+                            echo $PASS | sudo -E -S resize2fs  /dev/mapper/$UUID
+                            NID=$(notify-send  -r $NID -p "Wrapping UP")
+                            echo $PASS | sudo -S  cryptsetup luksClose "$UUID"
+                            echo 100
+                            fi 
+                        fi
+                    else 
+                        dorightthing
+                    fi
+
+                fi
+                if [[ "$DONT_CHANGE_WELCOME" != "0" ]] ; then 
+                    DONT_CHANGE_WELCOME=0
+                elif [[ "$Welcome" != "" ]];then 
+                    Welcome="Choose Another Action or Exit"
+                fi
+            fi
+
+        }
+
+        if [ $UID -eq 0 ];then
+            USER_I=$1
+            VAULTS=$2
+            FOLDER="/home/$USER_I/$VAULT_FOLDER"
+            PIPE="$FOLDER/pipe"
+            M_PIPE="$FOLDER/pipe/m_pipe"
+            if [ $1 == 0 ];then 
+                exit 1
+            fi
+            echo "Opening vault"
+            open
+            echo "Done opening vault"
+            exit 0
+        fi
+        trap close  SIGTERM SIGINT EXIT
+        if [[ ! -d $FOLDER ]];then
+            if [ $UID -eq 0 ];then 
+                echo "Run this as user, not root"
+            fi 
+            echo "Taking it as a new installation"
+            echo "Creating new folder $FOLDER"
+            echo "This will be location of your vaults"
+            if [[ "$PASS" == "" ]];
+            then 
+                PASS="$(zenity --title="Vaults" --title="Please enter your sudo password"  --password)"
+            fi
+            echo $PASS | sudo -S echo "Checking if sudo password is correct"
+            result=$?
+            if [[ "$result" != "0" ]];then 
+                zenity --title="Vault" --warning --text="Wrong Sudo Password Was Entered, Exiting"
+                exit 0
+            fi 
+
+            newinstall
+            fi
+            O_VAULT=()
+            mkdir $PIPE
+            mkfifo $M_PIPE
+            cd $FOLDER
+            echo "Welcome to your vaults"
+            Welcome=""
+            while true; do
+                if [[ "$Welcome" == "" ]];
+                then
+                    Welcome="Welcome to vaults"
+                fi
+                if [[ "$PASS" == "" ]];
+                then 
+                    PASS="$(zenity --title="Vaults" --title="Please enter your sudo password"  --password)"
+                    echo $PASS | sudo -S echo "Checking if sudo password is correct"
+                    result=$?
+                    if [[ "$result" != "0" ]];then 
+                        zenity --title="Vault" --warning --text="Wrong Sudo Password Was Entered, Exiting"
+                        exit 0
+                    fi 
+                    fi
+                    display
+                done 
